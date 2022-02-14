3e281f2d388f66c6ffc88bb0c0b5e220650185f6
Weslen Mendes <weslenmends@gmail.com>
2022-02-14 15:50:02 -0300

    Added homepage, sidebar in layout and participant loading

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 let messages = null;
 
+let allUsers = [];
+
 const userInfo = {
   name: "todos",
   temporaryUsername: "todos",
   idIntervalUpdate: null,
   idIntervalKeepLoggedIn: null,
+  idIntervalGetUsers: null,
+};
+
+const messageObj = {
+  from: userInfo.name,
+  to: "todos",
+  text: "",
+  type: "message",
 };
 
 function getMessages() {
@@ -20,7 +30,7 @@ function requestWorked(res) {
 
   if (checkIfMessagesHaveChanged(res.data)) {
     allMessagesToHTML = createHTMLToMessages();
-    messagesIntoHTML(allMessagesToHTML);    
+    messagesIntoHTML(allMessagesToHTML);
   }
 }
 
@@ -34,29 +44,31 @@ function createHTMLToMessages() {
   let allHTML = "";
   let HTMLMessage = null;
   let classMessage;
-  
+
   if (messages) {
     messages.forEach((message) => {
       HTMLMessage = "";
-      const privateMessageCondition = message.type === "private_message" && (message.to.toLowerCase() === userInfo.name.toLowerCase() || message.from.toLowerCase() === userInfo.name.toLowerCase());
+      const privateMessageCondition =
+        message.type === "private_message" &&
+        (message.to === userInfo.name || message.from === userInfo.name);
 
       if (message.type === "status") {
         classMessage = "message status-message";
         HTMLMessage = `<div class="${classMessage}" data-identifier="message">
-        <p><span>${message.time}</span> <em>${message.from}</em> ${message.text}</p>
+        <p><span>(${message.time})</span> &nbsp;<em>${message.from}</em> &nbsp;${message.text}</p>
         </div>`;
       } else if (privateMessageCondition) {
         classMessage = "message reserved";
         HTMLMessage = `<div class="${classMessage}" data-identifier="message">
-        <p><span>${message.time}</span> <em>${message.from}</em> reservadamente para <em>${message.to}</em>: ${message.text}</p>
+        <p><span>(${message.time})</span> &nbsp;<em>${message.from}</em> reservadamente para <em>${message.to}</em>: &nbsp;${message.text}</p>
         </div>`;
       } else {
         classMessage = "message";
         HTMLMessage = `<div class="${classMessage}" data-identifier="message">
-        <p><span>${message.time}</span> <em>${message.from}</em> para <em>${message.to}</em>: ${message.text}</p>
+        <p><span>(${message.time})</span> &nbsp;<em>${message.from}</em> para <em>${message.to}</em>: &nbsp;${message.text}</p>
         </div>`;
       }
-      
+
       allHTML += HTMLMessage;
     });
   }
@@ -65,9 +77,10 @@ function createHTMLToMessages() {
 }
 
 function messagesIntoHTML(html) {
-  let sectionMessages = document.querySelector(".messages");  
+  let sectionMessages = document.querySelector(".messages");
   sectionMessages.innerHTML = html;
 
+  addClickEventToSidebar();
   addKeyDownEvent();
   scrollToLastMessage();
 }
@@ -84,12 +97,12 @@ function checkIfMessagesHaveChanged(data) {
 function scrollToLastMessage() {
   const allMessages = document.querySelectorAll(".message");
   const lastMessage = allMessages[allMessages.length - 1];
-  
+
   lastMessage.scrollIntoView({ behavior: "smooth" });
 }
 
 function registerUser(username) {
-  const URL = "https://mock-api.driven.com.br/api/v4/uol/participants ";
+  const URL = "https://mock-api.driven.com.br/api/v4/uol/participants";
   const request = axios.post(URL, { name: username });
 
   userInfo.temporaryUsername = username;
@@ -100,19 +113,36 @@ function registerUser(username) {
 
 function registerSuccess() {
   userInfo.name = userInfo.temporaryUsername;
+  messageObj.from = userInfo.name;
 
+  getUsers();
   getMessages();
+  showLayout();
+  removeInitialPage();
   keepUserLoggedIn();
-  
+
+  userInfo.idIntervalGetUsers = setInterval(getUsers, 10000);
   userInfo.idIntervalUpdate = setInterval(getMessages, 3000);
   userInfo.idIntervalKeepLoggedIn = setInterval(keepUserLoggedIn, 5000);
 }
 
 function registerFailed(error) {
-  console.log("Error: " + error.response.status);
-  console.log("Message: " + error.response.data);
+  const section = document.querySelector(".initial-page main section");
+  const loading = document.querySelector(".loading");
+  const alertMessage = document.querySelector(".alert-message .content");
 
-  init();
+  if (section && loading && alertMessage) {
+    loading.classList.add("hidden");
+    section.classList.remove("hidden");
+
+    if (error.response.status === 400) {
+      alertMessage.innerHTML = `O usuário com nome <em>${userInfo.temporaryUsername}</em> já existe no sistema!`;
+    }
+
+    if (error.response.status === 500) {
+      alertMessage.innerText = `Ocorreu um erro no servidor, por favor tente novamente mais tarde!`;
+    }
+  }
 }
 
 function keepUserLoggedIn() {
@@ -138,20 +168,20 @@ function addKeyDownEvent() {
 function sendMessage() {
   const input = document.querySelector(".input");
   const inputText = input.value;
-  const messageTo = "Weslen";
-  const messageObj = {
-    from: userInfo.name,
-    to: messageTo,
-    text: inputText,
-    type: messageTo !== "todos" ? "private_message" : "message",
-  };
+
+  messageObj.text = inputText;
 
   if (inputText) {
     const URL = "https://mock-api.driven.com.br/api/v4/uol/messages";
     const request = axios.post(URL, messageObj);
 
     input.value = "";
-    request.then(() => getMessages());
+    request.then(
+      () => getMessages(),
+      (e) => {
+        alert(e);
+      }
+    );
     request.catch(() => window.location.reload());
   }
 }
@@ -163,12 +193,247 @@ function focusInput() {
 function quit() {
   clearInterval(userInfo.idIntervalUpdate);
   clearInterval(userInfo.idIntervalKeepLoggedIn);
+  clearInterval(userInfo.idIntervalGetUsers);
+}
+
+function showLayout() {
+  const contentHidden = document.querySelectorAll(".hidden");
+  const initialPage = document.querySelector(".initial-page");
+  let tagName = "";
+  let condition = null;
+
+  contentHidden.forEach((content) => {
+    tagName = content.tagName.toLowerCase();
+    condition =
+      tagName === "header" || tagName === "main" || tagName === "footer";
+
+    if (condition) {
+      content.classList.remove("hidden");
+    }
+  });
+
+  initialPage.classList.add("hidden");
+}
+
+function showLoading() {
+  const section = document.querySelector(".initial-page main section");
+  const loading = document.querySelector(".loading.hidden");
+
+  if (loading && section) {
+    section.classList.add("hidden");
+    loading.classList.remove("hidden");
+  }
+}
+
+function tryLogIn() {
+  const inputName = document.querySelector(".initial-page .input-name");
+  const alertMessage = document.querySelector(".alert-message .content");
+  const name = inputName.value;
+  const id = null;
+
+  alertMessage.innerText = "";
+
+  if (name) {
+    showLoading();
+    registerUser(name);
+  }
+
+  return () => clearTimeout(id);
+}
+
+function showUsersInSidebar() {
+  const users = document.querySelector(".users");
+  let className = "user checked";
+  let hidden = "";
+
+  if (!allUsers.includes(messageObj.to)) {
+    messageObj.to = "todos";
+  }
+
+  if (messageObj.to !== "todos") {
+    className = "user";
+    hidden = "hidden";
+  }
+
+  users.innerHTML = `<div class="${className}" data-identifier="participant">
+    <div class="info">
+      <ion-icon name="people"></ion-icon>
+      <p>Todos</p>
+    </div>
+
+    <div class="${hidden}">
+      <img src="./assets/check.svg"/>
+    </div>
+  </div>`;
+
+  allUsers.forEach((user) => {
+    if (user === messageObj.to) {
+      className = "user checked";
+      hidden = "";
+    } else {
+      className = "user";
+      hidden = "hidden";
+    }
+
+    users.innerHTML += `<div class="${className}" data-identifier="participant">
+      <div class="info">
+        <ion-icon name="person-circle"></ion-icon>
+        <p>${user}</p>
+      </div>
+
+      <div class="${hidden}">
+        <img src="./assets/check.svg"/>
+      </div>      
+    </div>`;
+  });
+}
+
+function addClickEventToSidebar() {
+  const users = document.querySelectorAll(".sidebar .user");
+  const option1 = document.querySelector(".sidebar .option-1");
+  const option2 = document.querySelector(".sidebar .option-2");
+
+  if (users) {
+    users.forEach((user) => {
+      user.addEventListener("click", chooseUser);
+    });
+  }
+
+  if (option1 && option2) {
+    option1.addEventListener("click", chooseVisibility);
+    option2.addEventListener("click", chooseVisibility);
+  }
+}
+
+function chooseUser(e) {
+  const userChecked = document.querySelector(".user.checked");
+  const sendTo = document.querySelector(".input-container .send-to");
+  let iconChecked = userChecked.querySelector("div:last-child");
+  let userName = "";
+  const visibility = messageObj.type === "message" ? "Público" : "Reservado";
+  let receiver = "";
+
+  if (userChecked && sendTo) {
+    userChecked.classList.remove("checked");
+    iconChecked.classList.add("hidden");
+
+    this.classList.add("checked");
+    iconChecked = this.querySelector(".hidden");
+    iconChecked.classList.remove("hidden");
+
+    userName = this.querySelector(".info p").innerText;
+    messageObj.to = userName;
+
+    receiver = capitalize(messageObj.to);
+
+    sendTo.innerHTML =
+      visibility === "Público"
+        ? `Enviando para <span class="to">${receiver}</span> <span class="visibility">(Publicamente)</span>`
+        : `Enviando para <span class="to">${receiver}</span> <span class="visibility">(Reservadamente)</span>`;
+  }
+}
+
+function chooseVisibility(e) {
+  const visibilityChecked = document.querySelector(".options .checked");
+  const sendTo = document.querySelector(".input-container .send-to");
+  let iconChecked = visibilityChecked.querySelector("div:last-child");
+  let visibility = "";
+  let receiver = "";
+
+  if (visibilityChecked && sendTo) {
+    visibilityChecked.classList.remove("checked");
+    iconChecked.classList.add("hidden");
+
+    this.classList.add("checked");
+    iconChecked = this.querySelector(".hidden");
+    iconChecked.classList.remove("hidden");
+
+    receiver = capitalize(messageObj.to);
+
+    visibility = this.querySelector("p").innerText;
+    sendTo.innerHTML =
+      visibility === "Público"
+        ? `Enviando para <span class="to">${receiver}</span> <span class="visibility">(Publicamente)</span>`
+        : `Enviando para <span class="to">${receiver}</span> <span class="visibility">(Reservadamente)</span>`;
+    messageObj.type = visibility === "Público" ? "message" : "private_message";
+  }
+}
+
+function showSidebar() {
+  const background = document.querySelector(".background");
+  const sidebar = document.querySelector(".sidebar");
+
+  if (background && sidebar) {
+    background.classList.remove("hidden");
+    sidebar.classList.add("show");
+  }
+}
+
+function closeSidebar() {
+  const background = document.querySelector(".background");
+  const sidebar = document.querySelector(".sidebar");
+
+  if (background && sidebar) {
+    background.classList.add("hidden");
+    sidebar.classList.remove("show");
+  }
+}
+
+function removeInitialPage() {
+  const initialPage = document.querySelector(".initial-page");
+  let id;
+
+  if (initialPage) {
+    initialPage.classList.add("hidden");
+
+    id = setTimeout(() => {
+      const initialPage = document.querySelector(".initial-page.hidden");
+      initialPage.remove();
+    }, 3000);
+  }
+
+  return () => clearTimeout(id);
+}
+
+function addAnimationInInitialPage() {
+  let initialPage = document.querySelector(".initial-page");
+  let id = null;
+
+  id = setTimeout(() => {
+    if (initialPage) {
+      initialPage.classList.add("animation");
+    }
+  }, 500);
+
+  return () => clearTimeout(id);
+}
+
+function getUsers() {
+  const URL = "https://mock-api.driven.com.br/api/v4/uol/participants";
+  const req = axios.get(URL);
+
+  req.then((res) => {
+    allUsers = [];
+
+    res.data.forEach((participant) => {
+      allUsers.push(participant.name);
+    });
+
+    showUsersInSidebar();
+  });
+
+  req.catch((e) => {
+    console.log(e);
+  });
+}
+
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1, word.length);
 }
 
 function init() {
-  const name = prompt("Qual é o seu nome?");
-  
-  registerUser(name);
+  addAnimationInInitialPage();
+  getMessages();
 }
 
 window.onload = init;
